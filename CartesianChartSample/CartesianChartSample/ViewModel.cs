cab7d8ae7268c1a5b72faab8f116d5476177a8e5
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-11-29 13:22:37 +0530

    add exporting sample

---
+++ CartesianChartSample/CartesianChartSample/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartesianChartSample
+{
+    
+    internal class ViewModel
+    {
+        public ObservableCollection<Model> CartesianData { get; set; }
+
+        public ViewModel()
+        {
+            CartesianData = new ObservableCollection<Model>()
+            {
+                new Model { Name = "David", Height = 170, Weight = 65 },
+                new Model { Name = "Michael", Height = 96, Weight = 48 },
+                new Model { Name = "Steve", Height = 65, Weight = 45 },
+                new Model { Name = "Joel", Height = 182, Weight = 60 },
+                new Model { Name = "Bob", Height = 134, Weight = 50 },
+                new Model { Name = "Thomas", Height = 177, Weight = 60 },
+            };
+        }
+    };
+}
+
+
